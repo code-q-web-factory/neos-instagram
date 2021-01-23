2b9f24af29ff9e44875258220876733f1739b3b5
Roland Schütz <mail@rolandschuetz.at>
2021-01-23 17:40:21 +0100

    WIP: Added basic functions

--- Classes/CodeQ/Instagram/Eel/Instagram.php
+++ Classes/CodeQ/Instagram/Eel/Instagram.php
@@ -8,6 +8,18 @@ use Neos\Flow\Annotations as Flow;
  * Class Instagram
  * @package CodeQ\Instagram\Eel
  *
+ * This class can be used to create and refresh Instagram access tokens as well as to retrieve Instagram media files.
+ *
+ * First you have to setup a Facebook Instagram app. To do so follow the instructions up until
+ * "Step 5: Exchange the Code for a Token" at
+ * https://developers.facebook.com/docs/instagram-basic-display-api/getting-started
+ *
+ * Then you can create a long-lived access token by clicking on the button "Generate Token" in Facebook developer's
+ * "User Token Generator" or use the function getToken.
+ *
+ * The token has a lifespan of 60 days - to refresh the token you can use the function refreshToken.
+ *
+ * To retrieve Media files you can use the function getInstagramFeed.
  */
 class Instagram implements ProtectedContextAwareInterface {
 
@@ -21,6 +33,21 @@ class Instagram implements ProtectedContextAwareInterface {
     */
     public function injectSettings(array $settings) {
         $this->settings = $settings;
+    }
+
+    /**
+     * @param $token
+     * @return mixed
+     * @throws \Exception
+     */
+    public function refreshToken($token)
+    {
+        $apiData = [
+            'grant_type' => 'ig_refresh_token',
+            'access_token' => $token
+        ];
+
+        return $this->makeApiCall($this->settings['apiEndpoints']['tokenRefreshUrl'], $apiData, 'GET');
     }
 
     /**
@@ -43,6 +70,20 @@ class Instagram implements ProtectedContextAwareInterface {
         }
 
         return isset($result['data']) ? $result['data'] : [];
+    }
+
+    /**
+     * @param $code
+     * @param $appId
+     * @param $appSecret
+     * @param $redirectUri
+     * @return mixed
+     * @throws \Exception
+     */
+    public function getToken($code, $appId, $appSecret, $redirectUri)
+    {
+        $shortLivedToken = $this->getShortLivedToken($appId, $appSecret, $redirectUri, $code);
+        return $this->getLongLivedToken($shortLivedToken, $appSecret);
     }
 
     /**
@@ -84,6 +125,57 @@ class Instagram implements ProtectedContextAwareInterface {
 
         return json_decode($jsonData, 1);
     }
+
+    /**
+     * @param $token
+     * @param $appSecret
+     * @return mixed
+     * @throws \Exception
+     */
+    private function getLongLivedToken($token, $appSecret)
+    {
+        $apiData = [
+            'client_secret' => $appSecret,
+            'grant_type' => 'ig_exchange_token',
+            'access_token' => $token
+        ];
+
+        $result = $this->makeApiCall($this->settings['apiEndpoints']['tokenExchangeUrl'], $apiData, 'GET');
+
+        if (!isset($result->access_token)) {
+            throw new \Exception("Could not retrieve long lived access token: $result->error_message");
+        }
+
+        return $result->access_token;
+    }
+
+    /**
+     * @param $appId
+     * @param $appSecret
+     * @param $redirectUri
+     * @param $code
+     * @return mixed
+     * @throws \Exception
+     */
+    private function getShortLivedToken($appId, $appSecret, $redirectUri, $code)
+    {
+        $apiData = [
+            'app_id' => $appId,
+            'app_secret' => $appSecret,
+            'grant_type' => 'authorization_code',
+            'redirect_uri' => $redirectUri,
+            'code' => $code
+        ];
+
+        $result = $this->makeApiCall($this->settings['apiEndpoints']['oauthTokenUrl'], $apiData);
+
+        if (!isset($result->access_token)) {
+            throw new \Exception("Could not retrieve short lived access token: $result->error_message");
+        }
+
+        return $result->access_token;
+    }
+
 
     public function allowsCallOfMethod($methodName) {
         return true;
